8e31b4c165b55f3bca95f22a154510a28ef86813
Sujan <sujan.hyoju@hvnnet.com>
2022-04-09 22:35:23 +0545

    Not using spring-boot-starter-test dependency.

--- src/test/java/com/havocinc/restnlp/RestnlpApplicationTests.java
+++ src/test/java/com/havocinc/restnlp/RestnlpApplicationTests.java
@@ -1,12 +1,12 @@
 package com.havocinc.restnlp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RestnlpApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
